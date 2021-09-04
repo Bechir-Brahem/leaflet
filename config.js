9d6069bdf3c10b4e389a9076343588254b6dbfcf
Bechir-Brahem <bachbrahem@gmail.com>
2021-09-04 12:31:20 +0100

    better layout

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const env= process.env
 const config = {
-   db: { /* don't expose password or any sensitive info, done only for demo */
+   db: { 
      host: env.DB_HOST || 'sql3.freemysqlhosting.net',
      user: env.DB_USER || 'sql3434037',
      password: env.DB_PASSWORD || 'tryGseln3j',
      database: env.DB_NAME || 'sql3434037',
    },
+
 
   // listPerPage: env.LIST_PER_PAGE || 10,
 };
