a09d52ddbffde1ab40d0f7ce87cb1a0e4ad98a1a
Bechir-Brahem <bachbrahem@gmail.com>
2021-10-07 19:37:03 +0100

    a

--- app.js
+++ app.js
@@ -6,25 +6,31 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+// app.get('*', function (req, res) {
+//     console.log(req.path)
+// })
+
 app.set('view engine', 'twig');
 app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'js', 'landing.html'));
+})
 app.use('/static', express.static('public'))
 app.use('/api', apiRouter)
 app.use(express.static(path.resolve(__dirname, 'build2')));
 app.use(express.static(path.resolve(__dirname, 'build1')));
 
+app.get('/react1', (req, res) => {
+    console.log("react1")
+    res.sendFile(path.resolve(__dirname, 'build1', 'index.html'));
+});
 app.get('/react2', (req, res) => {
     console.log("react2")
     res.sendFile(path.resolve(__dirname, 'build2', 'index.html'));
 });
-app.get('/react1', (req, res) => {
-    console.log("react2")
-    res.sendFile(path.resolve(__dirname, 'build1', 'index.html'));
-});
 
 
-app.use('/', indexRouter);
 app.use('/js', indexRouter);
 
 
