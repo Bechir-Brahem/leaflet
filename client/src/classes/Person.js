9bd3c019e886f4b70a5ad3d8e676c178489dfafe
alabh <alabenhmouda@gmail.com>
2021-11-13 15:01:16 +0100

    Fix bug where date changes doesn't affect the markers and polylines of a person after toggling it

--- client/src/classes/Person.js
+++ client/src/classes/Person.js
@@ -1,5 +1,5 @@
-import {LayerGroup, Polyline, Popup} from "react-leaflet";
-import {numberToDateString, numberToTimeString} from "./Helper";
+import { LayerGroup, Polyline, Popup } from "react-leaflet";
+import { numberToDateString, numberToTimeString } from "./Helper";
 import React from "react";
 
 export class Person {
@@ -12,56 +12,59 @@ export class Person {
     /**
      * @type {Position[]}
      */
-    positions = []
-    markers = []
-    problems = []
+    positions = [];
+    markers = [];
+    problems = [];
     color;
     icons;
     oldMarkers = [];
     oldPolylines = [];
 
-
     update(state) {
         if (state.isShown === false) {
             this.oldMarkers = this.markers;
             this.oldPolylines = this.polylines;
-            this.markers = []
-            this.polylines = []
+            this.markers = [];
+            this.polylines = [];
             return;
         }
         if (state.isShown === true && this.oldMarkers.length !== 0) {
             this.markers = this.oldMarkers;
             this.polylines = this.oldPolylines;
+            this.oldMarkers = [];
+            this.oldPolylines = [];
             return;
         }
 
-        let lastPos = null
+        let lastPos = null;
         let tmpPolyline = [];
         let pos;
-        this.markers = []
-        this.polylines = []
-        this.problems = []
+        this.markers = [];
+        this.polylines = [];
+        this.problems = [];
         if (!state.isShown) return;
 
         for (pos of this.positions) {
-
             if (!this.checkDate(pos, state)) continue;
 
             let problem = false;
             if (lastPos && this.checkProblem(pos, lastPos)) {
-                this.insertPolyline(tmpPolyline, pos)
-                tmpPolyline = [[lastPos.lt, lastPos.lg], [pos.lt, pos.lg]];
-                this.insertPolyline(tmpPolyline, pos, "b", true, lastPos)
-                tmpPolyline = []
+                this.insertPolyline(tmpPolyline, pos);
+                tmpPolyline = [
+                    [lastPos.lt, lastPos.lg],
+                    [pos.lt, pos.lg],
+                ];
+                this.insertPolyline(tmpPolyline, pos, "b", true, lastPos);
+                tmpPolyline = [];
                 problem = true;
-                this.problems.push([pos, lastPos])
+                this.problems.push([pos, lastPos]);
             }
 
-            tmpPolyline.push([pos.lt, pos.lg])
-            this.markers.push(pos.getMarker(problem, this.name, this.icons))
-            lastPos = pos
+            tmpPolyline.push([pos.lt, pos.lg]);
+            this.markers.push(pos.getMarker(problem, this.name, this.icons));
+            lastPos = pos;
         }
-        this.insertPolyline(tmpPolyline, pos, "c")
+        this.insertPolyline(tmpPolyline, pos, "c");
     }
 
     getLayerGroup() {
@@ -69,9 +72,9 @@ export class Person {
             <LayerGroup key={this.name}>
                 {this.markers}
                 {this.polylines}
-            </LayerGroup>)
+            </LayerGroup>
+        );
     }
-
 
     /**
      * checks if the time differance between two positions is more than 2 hours
@@ -81,16 +84,22 @@ export class Person {
      */
     checkProblem(position, lastPos) {
         // combines pos.DA and pos.TI to make a Date object
-        let posDate = new Date(numberToDateString(position.date, 1) + " " + numberToTimeString(position.time));
-        let lastPosDate = new Date(numberToDateString(lastPos.date, 1) + " " + numberToTimeString(lastPos.time));
+        let posDate = new Date(
+            numberToDateString(position.date, 1) +
+                " " +
+                numberToTimeString(position.time)
+        );
+        let lastPosDate = new Date(
+            numberToDateString(lastPos.date, 1) +
+                " " +
+                numberToTimeString(lastPos.time)
+        );
 
         //diff contains the difference between the two positions in milliseconds
         let diff = lastPosDate - posDate;
         // contains the difference in minutes
-        diff = diff / 1000 / 60
+        diff = diff / 1000 / 60;
         return diff > 120;
-
-
     }
 
     /**
@@ -101,27 +110,40 @@ export class Person {
      * @param {boolean} problem is the current polyline in a state of a problem?
      * @param {Position} lastPos  last position
      */
-    insertPolyline(tmpPolyline, pos, ref = "a", problem = false, lastPos = null) {
+    insertPolyline(
+        tmpPolyline,
+        pos,
+        ref = "a",
+        problem = false,
+        lastPos = null
+    ) {
         /** if array contains less than one item than it can't form a polyline */
         if (tmpPolyline.length <= 1) return;
 
-        let obj, text
+        let obj, text;
         /** generates obj which contains information about the type of line
          * and text which is the inner text if the popup
          */
         if (problem) {
-            obj = {color: 'red', dashArray: "20,20"}
-            text = <span>
-                <center>{this.name}</center>
-                ({pos.lt} , {pos.lg} ) => ({lastPos.lt} , {lastPos.lg})
-                <br/>
-                {"(" + numberToDateString(pos.date) + " , " + numberToTimeString(pos.time) +
-                ") => ("
-                + numberToDateString(lastPos.date) + " , " + numberToTimeString(lastPos.time) + ")"}
-            </span>
-
+            obj = { color: "red", dashArray: "20,20" };
+            text = (
+                <span>
+                    <center>{this.name}</center>({pos.lt} , {pos.lg} ) => (
+                    {lastPos.lt} , {lastPos.lg})
+                    <br />
+                    {"(" +
+                        numberToDateString(pos.date) +
+                        " , " +
+                        numberToTimeString(pos.time) +
+                        ") => (" +
+                        numberToDateString(lastPos.date) +
+                        " , " +
+                        numberToTimeString(lastPos.time) +
+                        ")"}
+                </span>
+            );
         } else {
-            obj = {color: this.color};
+            obj = { color: this.color };
             text = <center>{this.name}</center>;
         }
 
@@ -130,13 +152,11 @@ export class Person {
             <Polyline
                 key={pos.id + ref}
                 pathOptions={obj}
-                positions={tmpPolyline}>
-                <Popup>
-                    {text}
-                </Popup>
+                positions={tmpPolyline}
+            >
+                <Popup>{text}</Popup>
             </Polyline>
         );
-
     }
 
     checkDate(pos, state) {
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,67 +1,68 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import Map from "./Map";
-import {Container, Row} from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Color, generateBoatIcon} from "../classes/Helper";
-import {Position} from "../classes/Position";
-import {Person} from "../classes/Person";
+import { Color, generateBoatIcon } from "../classes/Helper";
+import { Position } from "../classes/Position";
+import { Person } from "../classes/Person";
 import RightPanel from "./RightPanel";
 
 Date.prototype.yyyymmdd = function () {
     const mm = this.getMonth() + 1; // getMonth() is zero-based
     const dd = this.getDate();
 
-    return [this.getFullYear(),
-        (mm > 9 ? '' : '0') + mm,
-        (dd > 9 ? '' : '0') + dd
-    ].join('-');
+    return [
+        this.getFullYear(),
+        (mm > 9 ? "" : "0") + mm,
+        (dd > 9 ? "" : "0") + dd,
+    ].join("-");
 };
 
 export let usefulValues = [];
 
-let peopleState = [];
-let people = [];
+let peopleState = {};
+let people = {};
 
 class App extends Component {
     state = {
-        peopleState: []
-    }
-
+        peopleState: {},
+    };
 
     componentDidMount() {
-        console.log("start ComponentDidMount", Date.now())
+        console.log("start ComponentDidMount", Date.now());
         axios.get("/api").then((res) => {
-                // people = []
-                console.log("start axios", Date.now())
-                let colorGen = new Color();
-                for (let pos of res.data) {
-                    if (people[pos.na] === undefined) {
-                        people[pos.na] = new Person(pos.na)
-                        let color = colorGen.getNext()
+            // people = []
+            console.log("start axios", Date.now());
+            let colorGen = new Color();
+            for (let pos of res.data) {
+                if (people[pos.na] === undefined) {
+                    people[pos.na] = new Person(pos.na);
+                    let color = colorGen.getNext();
 
-                        people[pos.na].icons = [generateBoatIcon(color), generateBoatIcon(color, "red")];
-                        people[pos.na].color = color
+                    people[pos.na].icons = [
+                        generateBoatIcon(color),
+                        generateBoatIcon(color, "red"),
+                    ];
+                    people[pos.na].color = color;
 
-                        peopleState[pos.na] = {
-                            isShown: true,
-                            startDate: new Date("2021-01-01"),
-                            endDate: new Date("2022-01-01"),
-                        }
-
-                    }
-                    people[pos.na].positions.push(new Position(pos))
+                    peopleState[pos.na] = {
+                        isShown: true,
+                        startDate: new Date("2021-01-01"),
+                        endDate: new Date("2022-01-01"),
+                    };
                 }
-                console.log(people)
-                Object.values(people).map(person => person.update({
+                people[pos.na].positions.push(new Position(pos));
+            }
+            Object.values(people).forEach((person) =>
+                person.update({
                     isShown: true,
                     startDate: new Date("2021-01-01"),
                     endDate: new Date("2022-01-01"),
-                }))
-                this.setState({peopleState: peopleState})
-            }
-        )
-
+                })
+            );
+            this.setState({ peopleState: peopleState });
+        });
     }
 
     /**
@@ -69,45 +70,39 @@ class App extends Component {
      * @param {String} name the name of the person to hide or show
      */
     togglePerson(name) {
-        console.log(name)
+        console.log(name);
         let tmp = Object.assign({}, this.state.peopleState);
         tmp[name].isShown = !tmp[name].isShown;
 
         people[name].update(tmp[name]);
-        this.setState({peopleState: tmp});
+        this.setState({ peopleState: tmp });
     }
 
     setDate(name, type, a) {
         let tmp = Object.assign({}, this.state.peopleState);
-        if (type === 0)
-            tmp[name].startDate = new Date(a);
-        else
-            tmp[name].endDate = new Date(a);
-        people[name].update(tmp[name])
-        this.setState({layerGroups: tmp})
-
-
+        if (type === 0) tmp[name].startDate = new Date(a);
+        else tmp[name].endDate = new Date(a);
+        people[name].update(tmp[name]);
+        this.setState({ layerGroups: tmp });
     }
 
     render() {
-        console.log("start render", Date.now())
-        /**
-         *
-         * @type {Person[]}
-         */
-        let problems = [];
-        Object.values(people).map((person) => {
-            problems[person.name] = person.problems
-        })
-
+        console.log("start render", Date.now());
+        let problems = {};
+        Object.values(people).forEach((person) => {
+            problems[person.name] = person.problems;
+        });
 
         return (
             <div>
                 <Container>
                     <Row>
-                        <Map people={people} peopleState={this.state.peopleState}/>
+                        <Map
+                            people={people}
+                            peopleState={this.state.peopleState}
+                        />
                     </Row>
-                    <Row style={{padding: "0!important"}}>
+                    <Row style={{ padding: "0!important" }}>
                         <RightPanel
                             peopleState={peopleState}
                             togglePerson={this.togglePerson.bind(this)}
