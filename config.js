5a6803cd88c6c4c9152963ff23bcf869b9d69c6f
Bechir-Brahem <bachbrahem@gmail.com>
2021-11-22 02:29:09 +0100

    build with limited api

--- config.js
+++ config.js
@@ -13,4 +13,3 @@ const pgConfig = {
 module.exports = {
     pgConfig
 };
-
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const {getMultiple} = require("../services/boats");
+const {getMultiple, getLimited} = require("../services/boats");
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-
-
     return res.json(await getMultiple())
+});
+router.get('/2', async function (req, res, next) {
+    return res.json(await getLimited(20))
 });
 
 module.exports = router;
